d21484556d1b9034b338396fecef5e40b843c126
Abdoulaye SARR <dadscientific@hotmail.com>
2019-10-29 11:45:39 +0100

    Add & delete todo Ok

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {TodoItemData} from "../dataTypes/TodoItemData";
+import {TodoService} from "../todo.service";
 
 @Component({
   selector: 'app-todo-item',
@@ -7,10 +9,48 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoItemComponent implements OnInit {
+  /**On récupère la todoItem passée depuis la composante mère**/
+  @Input()
+  private data: TodoItemData;
 
-  constructor() { }
+  /**Edition du label**/
+  labelChanging: boolean;
+
+  /**On injecte le service TodoService**/
+  constructor(private todoService: TodoService) {
+    this.labelChanging = false;
+  }
 
   ngOnInit() {
   }
 
+  /**On récupère le label du todoItem **/
+  get label(){
+    return this.data.label;
+  }
+
+  /**On récupère l'état du todoItem (fait ou pas fait) **/
+  get isDone(){
+    return this.data.isDone;
+  }
+
+  /**On change l'état du todoItem **/
+  itemDone(item: TodoItemData, done: boolean){
+    this.todoService.setItemsDone(done, item);
+  }
+
+  /**Suppression du todoItem**/
+  removeItem(data: TodoItemData) {
+    this.todoService.removeItems(data);
+  }
+
+  /**On change le label du todoItem **/
+  itemLabel(item: TodoItemData, label: string){
+    this.todoService.setItemsLabel(label, item);
+    this.labelChanging = false;
+  }
+  /**Passe l'édition du label à true**/
+  edit(){
+    this.labelChanging = true;
+  }
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -10,23 +10,56 @@ import {TodoService} from '../todo.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoListComponent implements OnInit {
-
-  @Input() 
-  
+  private titre: string;
+  @Input()
   private data: TodoListData;
-  
-  constructor(private todoService: TodoService) { 
+
+  constructor(private todoService: TodoService) {
+    todoService.getTodoListDataObserver().subscribe(tdl => this.data = tdl); /**Appel du service pour récupérer le TodoList**/
+    this.titre = this.data.label;  /**Titre du TodoList**/
   }
 
   ngOnInit() {
   }
 
+  /**Récupère le titre du TodoList**/
   get label(): string {
     return this.data ? this.data.label : '';
   }
 
+  /**Récupère le tableau des todoItems**/
   get items(): TodoItemData[] {
     return this.data ? this.data.items : [];
   }
 
+  /**Récupère le nombre des todoItems pas encore faits**/
+  get itemsRestantes(): number {
+    return this.items.reduce((acc, item) => acc + (item.isDone ? 0 : 1), 0);
+  }
+
+  /**Ajout d'un todoItem**/
+  appendItem(label: string){
+    this.todoService.appendItems(
+      {label, isDone: false}
+    );
+  }
+
+  /**On change l'état du todoItem **/
+  itemDone(item: TodoItemData, done: boolean){
+    this.todoService.setItemsDone(done, item);
+  }
+
+  /**On change l'état des todoItems **/
+  itemsDone(items: TodoItemData[], done: boolean){
+    this.todoService.setItemsDone(done, ...items);
+  }
+  /**On change le label du todoItem **/
+  itemLabel(item: TodoItemData, label: string){
+    this.todoService.setItemsLabel(label, item);
+  }
+
+  /**Suppression du todoItem**/
+  removeItem(item: TodoItemData){
+    this.todoService.removeItems(item);
+  }
 }
