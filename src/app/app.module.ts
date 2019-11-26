582ab28454db267c37d76655b3b36aa3388717a3
Abdoulaye SARR <dadscientific@hotmail.com>
2019-11-26 19:47:42 +0100

    NavBar authentification for firebase && update style css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,32 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import {TodoService} from './todo.service';
-import {FormsModule} from '@angular/forms';
+import {AuthService} from './services/auth.service';
+import {AuthGuardService} from './services/auth-guard.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'todolist', component: TodoListComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    SignupComponent,
+    SigninComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes)
   ],
-  providers: [TodoService],
+  providers: [TodoService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
