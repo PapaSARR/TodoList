810bbbfb5e7fac5fe48af9351b7678e5876b2646
Abdoulaye SARR <dadscientific@hotmail.com>
2019-11-26 23:33:07 +0100

    Connexion & inscription Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {TodoService} from './todo.service';
-import {TodoListData} from './dataTypes/TodoListData';
-import {TodoItemData} from './dataTypes/TodoItemData';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyAMaYlPyHjyYadvB7Q7Lqx-Gxf66uiR6rg",
+      authDomain: "todolist-a7191.firebaseapp.com",
+      databaseURL: "https://todolist-a7191.firebaseio.com",
+      projectId: "todolist-a7191",
+      storageBucket: "todolist-a7191.appspot.com",
+      messagingSenderId: "364186250710",
+      appId: "1:364186250710:web:de9146a1772c5bf4a1c877"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+}
